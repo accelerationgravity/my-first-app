0cc24c7ce0f585fc41982819520d39d24a2668eb
accelerationgravity <140845441+accelerationgravity@users.noreply.github.com>
2023-08-01 11:16:58 -0600

    created header and footer

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import App from './App';
+
+function Footer(props) {
+    return (
+        <>
+            <footer>
+                <h2>Copyright 🎉</h2>
+            </footer>
+
+        </>
+    );
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Header(props) {
+    return (
+        <>
+            <h1>Joe Blow's Website</h1>
+        </>
+    );
+}
+
+export default Header;
